b92d46de63964ba07a6c50ec694e4933aa1b0c4b
1GabrielBastos <biellclashbastos@gmail.com>
2025-03-13 14:29:31 -0300

    Primeiro commit

---
+++ src/main/java/com/example/aulajpa1/Aulajpa1Application.java
@@ -0,0 +1,13 @@
+package com.example.aulajpa1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aulajpa1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Aulajpa1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/aulajpa1/Aulajpa1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aulajpa1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Aulajpa1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
